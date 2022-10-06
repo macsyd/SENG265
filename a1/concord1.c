05d67558555516bec090426b0f88b07034bec917
Sydney Macdonald <sydneymacdonald@uvic.ca>
2022-10-06 14:42:58 -0700

    added array bound definitions

--- a1/concord1.c
+++ a1/concord1.c
@@ -1,1 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_NUM = 100;
+#define MAX_LINE_LENGTH = 70;
+#define MAX_EXCL_WORD_NUM = 20;
+#define MAX_EXCL_WORD_LENGTH = 20;
