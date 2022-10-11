95afb731d844ef35b12cc2bc23ff2e68092a091b
Sydney Macdonald <sydneymacdonald@uvic.ca>
2022-10-10 22:40:50 -0700

    started exclusion words

--- a1/concord1.c
+++ a1/concord1.c
@@ -6,3 +6,12 @@
 #define MAX_LINE_LENGTH = 70;
 #define MAX_EXCL_WORD_NUM = 20;
 #define MAX_EXCL_WORD_LENGTH = 20;
+
+int main(){
+	char *excl_words[MAX_EXCL_WORD_NUM][MAX_EXCL_WORD_LENGTH];
+
+}
+
+void get_exclusion_words(char *excl){
+	fgets(excl, MAX_EXCL_WORD_LENGTH, stdin);
+}
