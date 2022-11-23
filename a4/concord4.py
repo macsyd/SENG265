80bc2cd22f816f883a79f81787c874544c53a4f1
Sydney Macdonald <sydneymacdonald@uvic.ca>
2022-11-23 11:04:41 -0800

    created source files

---
+++ a4/concord4.py
@@ -0,0 +1,12 @@
+import sys
+import re
+
+class concord:
+
+    def __init__(self, input=None, output=None):
+        self.input = input
+        self.output = output
+        # self.determine_index_words()
+
+    def full_concordance(self):
+        return []
